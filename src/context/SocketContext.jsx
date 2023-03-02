1817fc463d3ab6b7be890ac5b0afe4e1b852c973
Gustavo Fonseca <gusfonseca.dev@gmail.com>
2023-03-02 15:46:47 -0300

    Resolução de bugs

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -1,8 +1,7 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:3000");
-// const socket = io.connect("https://rolezeiros-jogo-da-velha-server.glitch.me");
+const socket = io.connect("https://rolezeiros-jogo-da-velha-server.glitch.me");
 
 export const SocketContext = createContext();
 
